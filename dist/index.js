1b4d429f86240da582a97e0224e2994e0805aba7
meael <colddave@ya.ru>
2021-04-24 20:02:09 +0300

    feat: dev mode & new wrapper

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,14 @@ require('./sourcemap-register.js');module.exports =
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 6596:
+/***/ ((module) => {
+
+"use strict";
+module.exports = JSON.parse("{\"inputs\":{\"jiraHost\":\"somepurchases.atlassian.net\",\"jiraEmail\":\"somepurchases@icloud.com\",\"jiraToken\":\"zxLsnifc48uIHPaEmqaGA818\",\"githubToken\":\"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\"},\"overrides\":{\"github\":{\"context\":{\"payload\":{\"pull_request\":{\"number\":16,\"base\":{\"repo\":{\"name\":\"test-action\",\"owner\":{\"login\":\"meael\"}}}}}}}}}");
+
+/***/ }),
+
 /***/ 2932:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -9,51 +17,72 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const JiraClient = __nccwpck_require__(9252);
 
+const isDevMode = process.argv.slice(2)[0] === '--dev';
+
+let config = {
+  jiraHost: core.getInput("jiraHost"),
+  jiraEmail: core.getInput("jiraEmail"),
+  jiraToken: core.getInput("jiraToken"),
+  githubToken: core.getInput("githubToken"),
+}
+
+if (isDevMode) {
+  try {
+    const { overrides, inputs } = __nccwpck_require__(6596);
+    config = { ...config, ...inputs };
+    github.context.payload = overrides.github.context.payload
+  } catch (err) {
+    console.error('dev config error: ', err.message);
+  }
+}
+
 const parseJiraIssueKey = (value) => {
   const reg = /((?!([A-Z0-9a-z]{1,10})-?$)[A-Z]{1}[A-Z0-9]+-[1-9][0-9]*)/g;
   const match = value.match(reg);
   return match ? match[0] : match;
 };
 
-const formatMessages = (messages) => {
+const formatCommitMessages = (messages) => {
   return messages
     .filter(Boolean)
     .map((message) => `- ${message.replace(/\n\n/g, ' ')}`)
     .join("\n");
 };
 
-const getUpdatedDescription = (a, b)=>{
-  const reg = /---(.*?|\r\n)*---/g;
-  const match = a.match(reg);
-  return match ? a.replace(match[0], b) : a + '\n' + b;
+const getUpdatedPullDescription = (pullDescription, pullChangelog) => {
+  const reg = /<!-- generated changes start -->(.*?|\n|\r\n)*<!-- generated changes end -->/g;
+  const match = pullDescription.match(reg);
+  return match ? pullDescription.replace(match[0], pullChangelog) : pullDescription + '\n' + pullChangelog;
 }
 
 (async () => {
   try {
-    const jiraHost = core.getInput("jiraHost");
-    const jiraEmail = core.getInput("jiraEmail");
-    const jiraToken = core.getInput("jiraToken");
-    const githubToken = core.getInput("githubToken");
+    const { jiraHost, jiraEmail, jiraToken, githubToken } = config;
+
+    const owner = github.context.payload.pull_request.base.repo.owner.login;
+    const repo = github.context.payload.pull_request.base.repo.name;
+    const pull_number = github.context.payload.pull_request.number;
 
     const octokit = github.getOctokit(githubToken);
 
     const { data: commits } = await octokit.pulls.listCommits({
-      owner: github.context.payload.pull_request.base.repo.owner.login,
-      repo: github.context.payload.pull_request.base.repo.name,
-      pull_number: github.context.payload.pull_request.number,
+      owner,
+      repo,
+      pull_number,
     });
 
     const jiraIssueKeys = [];
     const otherCommitMessages = [];
 
-    commits.forEach(({ commit }) => {
-      const jiraKey = parseJiraIssueKey(commit.message);
-      if (jiraKey) {
-        jiraIssueKeys.push(jiraKey);
+    commits.forEach(({ commit: { message } }) => {
+      const jiraIssueKey = parseJiraIssueKey(message);
+
+      if (jiraIssueKey && !jiraIssueKeys.includes(jiraIssueKey)) {
+        jiraIssueKeys.push(jiraIssueKey);
         return;
       }
 
-      otherCommitMessages.push(commit.message);
+      if (!otherCommitMessages.includes(message)) otherCommitMessages.push(message);
     });
 
     const jira = new JiraClient({
@@ -65,37 +94,35 @@ const getUpdatedDescription = (a, b)=>{
     });
 
     const jiraCommitMessages = await Promise.all(
-      [...new Set(jiraIssueKeys)].map(async (issueKey) => {
+      jiraIssueKeys.map(async (issueKey) => {
         try {
           const issue = await jira.issue.getIssue({ issueKey });
           return `<a href="https://${jiraHost}/browse/${issueKey}">${issueKey}</a>: ${issue.fields.summary}`;
         } catch (err) {
-          return;
+          return `${issueKey}: ${commits.filter((c) => c.includes(issueKey)).join("; ")}`
         }
       })
     );
 
-    const { data: { body: oldDescription } } = await octokit.pulls.get({
-      owner: github.context.payload.pull_request.base.repo.owner.login,
-      repo: github.context.payload.pull_request.base.repo.name,
-      pull_number: github.context.payload.pull_request.number,
+    const { data: { body: pullDescription } } = await octokit.pulls.get({
+      owner,
+      repo,
+      pull_number,
     });
 
-    const newDescription = `
----
-### Jira changes
-${formatMessages(jiraCommitMessages)}
-
-### Other changes
-${formatMessages(otherCommitMessages)}
----
-`;  
+    const pullChangelog = `
+<!-- generated changes start -->
+### Changelog
+${formatCommitMessages(jiraCommitMessages)}
+${formatCommitMessages(otherCommitMessages)}
+<!-- generated changes end -->
+`;
 
     await octokit.pulls.update({
       owner: github.context.payload.pull_request.base.repo.owner.login,
       repo: github.context.payload.pull_request.base.repo.name,
       pull_number: github.context.payload.pull_request.number,
-      body: getUpdatedDescription(oldDescription, newDescription),
+      body: getUpdatedPullDescription(pullDescription, pullChangelog),
     });
   } catch (err) {
     core.setFailed(err.message);
--- index.js
+++ index.js
@@ -2,51 +2,72 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const JiraClient = require("jira-connector");
 
+const isDevMode = process.argv.slice(2)[0] === '--dev';
+
+let config = {
+  jiraHost: core.getInput("jiraHost"),
+  jiraEmail: core.getInput("jiraEmail"),
+  jiraToken: core.getInput("jiraToken"),
+  githubToken: core.getInput("githubToken"),
+}
+
+if (isDevMode) {
+  try {
+    const { overrides, inputs } = require("./devConfig.json");
+    config = { ...config, ...inputs };
+    github.context.payload = overrides.github.context.payload
+  } catch (err) {
+    console.error('dev config error: ', err.message);
+  }
+}
+
 const parseJiraIssueKey = (value) => {
   const reg = /((?!([A-Z0-9a-z]{1,10})-?$)[A-Z]{1}[A-Z0-9]+-[1-9][0-9]*)/g;
   const match = value.match(reg);
   return match ? match[0] : match;
 };
 
-const formatMessages = (messages) => {
+const formatCommitMessages = (messages) => {
   return messages
     .filter(Boolean)
     .map((message) => `- ${message.replace(/\n\n/g, ' ')}`)
     .join("\n");
 };
 
-const getUpdatedDescription = (a, b)=>{
-  const reg = /---(.*?|\r\n)*---/g;
-  const match = a.match(reg);
-  return match ? a.replace(match[0], b) : a + '\n' + b;
+const getUpdatedPullDescription = (pullDescription, pullChangelog) => {
+  const reg = /<!-- generated changes start -->(.*?|\n|\r\n)*<!-- generated changes end -->/g;
+  const match = pullDescription.match(reg);
+  return match ? pullDescription.replace(match[0], pullChangelog) : pullDescription + '\n' + pullChangelog;
 }
 
 (async () => {
   try {
-    const jiraHost = core.getInput("jiraHost");
-    const jiraEmail = core.getInput("jiraEmail");
-    const jiraToken = core.getInput("jiraToken");
-    const githubToken = core.getInput("githubToken");
+    const { jiraHost, jiraEmail, jiraToken, githubToken } = config;
+
+    const owner = github.context.payload.pull_request.base.repo.owner.login;
+    const repo = github.context.payload.pull_request.base.repo.name;
+    const pull_number = github.context.payload.pull_request.number;
 
     const octokit = github.getOctokit(githubToken);
 
     const { data: commits } = await octokit.pulls.listCommits({
-      owner: github.context.payload.pull_request.base.repo.owner.login,
-      repo: github.context.payload.pull_request.base.repo.name,
-      pull_number: github.context.payload.pull_request.number,
+      owner,
+      repo,
+      pull_number,
     });
 
     const jiraIssueKeys = [];
     const otherCommitMessages = [];
 
-    commits.forEach(({ commit }) => {
-      const jiraKey = parseJiraIssueKey(commit.message);
-      if (jiraKey) {
-        jiraIssueKeys.push(jiraKey);
+    commits.forEach(({ commit: { message } }) => {
+      const jiraIssueKey = parseJiraIssueKey(message);
+
+      if (jiraIssueKey && !jiraIssueKeys.includes(jiraIssueKey)) {
+        jiraIssueKeys.push(jiraIssueKey);
         return;
       }
 
-      otherCommitMessages.push(commit.message);
+      if (!otherCommitMessages.includes(message)) otherCommitMessages.push(message);
     });
 
     const jira = new JiraClient({
@@ -58,37 +79,35 @@ const getUpdatedDescription = (a, b)=>{
     });
 
     const jiraCommitMessages = await Promise.all(
-      [...new Set(jiraIssueKeys)].map(async (issueKey) => {
+      jiraIssueKeys.map(async (issueKey) => {
         try {
           const issue = await jira.issue.getIssue({ issueKey });
           return `<a href="https://${jiraHost}/browse/${issueKey}">${issueKey}</a>: ${issue.fields.summary}`;
         } catch (err) {
-          return;
+          return `${issueKey}: ${commits.filter((c) => c.includes(issueKey)).join("; ")}`
         }
       })
     );
 
-    const { data: { body: oldDescription } } = await octokit.pulls.get({
-      owner: github.context.payload.pull_request.base.repo.owner.login,
-      repo: github.context.payload.pull_request.base.repo.name,
-      pull_number: github.context.payload.pull_request.number,
+    const { data: { body: pullDescription } } = await octokit.pulls.get({
+      owner,
+      repo,
+      pull_number,
     });
 
-    const newDescription = `
----
-### Jira changes
-${formatMessages(jiraCommitMessages)}
-
-### Other changes
-${formatMessages(otherCommitMessages)}
----
-`;  
+    const pullChangelog = `
+<!-- generated changes start -->
+### Changelog
+${formatCommitMessages(jiraCommitMessages)}
+${formatCommitMessages(otherCommitMessages)}
+<!-- generated changes end -->
+`;
 
     await octokit.pulls.update({
       owner: github.context.payload.pull_request.base.repo.owner.login,
       repo: github.context.payload.pull_request.base.repo.name,
       pull_number: github.context.payload.pull_request.number,
-      body: getUpdatedDescription(oldDescription, newDescription),
+      body: getUpdatedPullDescription(pullDescription, pullChangelog),
     });
   } catch (err) {
     core.setFailed(err.message);
