502de604ce8d439a4711b2b5b887d0d3273a43f4
meael <colddave@ya.ru>
2021-04-24 14:12:10 +0300

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+# Jira summary action
+
+This action creates Jira summary in pull request description
+
+## Usage details
+
+Your commits should include Jira ticket key  
+Example commit name: `FE-100 optional commit text`  
+*`FE-100` in example is ticket key from Jira*
+
+## Inputs
+
+Set it in `repository settings -> secrets`
+
+### `JIRA_TOKEN`
+**Required** Jira account api token  
+[You can create it here](https://id.atlassian.com/manage-profile/security/api-tokens)
+
+### `JIRA_EMAIL`
+**Required** Jira account email (Example: `example@example.com`)
+
+### `JIRA_HOST`
+**Required** Jira host (Example: `someworkspace.atlassian.net`)
+
+## Example usage
+
+Place code below in repository root `/.github/workflows/main.yml`
+
+```yaml
+on:
+  pull_request:
+    types: [opened, synchronize]
+
+jobs:
+  jira_summary_job:
+    runs-on: ubuntu-latest
+    name: A job to get pull request summary
+    steps:
+    - name: Jira summary
+      uses: meael/jira-summary-action@2.1
+      with:
+        githubToken: ${{ secrets.GITHUB_TOKEN }}
+        jiraHost: ${{ secrets.JIRA_HOST }}
+        jiraEmail: ${{ secrets.JIRA_EMAIL }}
+        jiraToken: ${{ secrets.JIRA_TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,106 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const JiraClient = require("jira-connector");
+
+const parseJiraIssueKey = (value) => {
+  const reg = /^((?!([A-Z0-9a-z]{1,10})-?$)[A-Z]{1}[A-Z0-9]+-[1-9][0-9]*)/g;
+  const match = value.match(reg);
+  return match ? match[0] : match;
+};
+
+const formatMessages = (messages) => {
+  return messages
+    .filter(Boolean)
+    .map((message) => `- ${message.replaceAll('\n\n', ' ')}`)
+    .join("\n");
+};
+
+const getUpdatedDescription = (a, b)=>{
+  const reg = /---(.*?|\r\n)*---/g;
+  const match = a.match(reg);
+  return match ? a.replace(match[0], b) : a + '\n' + b;
+}
+
+(async () => {
+  try {
+    const jiraHost = core.getInput("jiraHost");
+    const jiraEmail = core.getInput("jiraEmail");
+    const jiraToken = core.getInput("jiraToken");
+    const githubToken = core.getInput("githubToken");
+
+    const octokit = github.getOctokit(githubToken);
+
+    const { data: commits } = await octokit.pulls.listCommits({
+      owner: github.context.payload.pull_request.base.repo.owner.login,
+      repo: github.context.payload.pull_request.base.repo.name,
+      pull_number: github.context.payload.pull_request.number,
+    });
+
+    const jiraIssueKeys = [];
+    const mergeCommitMessages = [];
+    const otherCommitMessages = [];
+
+    commits.forEach(({ commit }) => {
+      const jiraKey = parseJiraIssueKey(commit.message);
+      if (jiraKey) {
+        jiraIssueKeys.push(jiraKey);
+        return;
+      }
+
+      if(commit.message.toLowerCase().includes('merge')){
+        mergeCommitMessages.push(commit.message);
+        return 
+      }
+
+      otherCommitMessages.push(commit.message);
+    });
+
+    const jira = new JiraClient({
+      host: jiraHost,
+      basic_auth: {
+        email: jiraEmail,
+        api_token: jiraToken,
+      },
+    });
+
+    const jiraCommitMessages = await Promise.all(
+      [...new Set(jiraIssueKeys)].map(async (issueKey) => {
+        try {
+          const issue = await jira.issue.getIssue({ issueKey });
+          return `<a href="https://${jiraHost}/browse/${issueKey}">${issueKey}</a>: ${issue.fields.summary}`;
+        } catch (err) {
+          return;
+        }
+      })
+    );
+
+    const { data: { body: oldDescription } } = await octokit.pulls.get({
+      owner: github.context.payload.pull_request.base.repo.owner.login,
+      repo: github.context.payload.pull_request.base.repo.name,
+      pull_number: github.context.payload.pull_request.number,
+    });
+
+    const newDescription = `
+
+---
+### Jira changes
+${formatMessages(jiraCommitMessages)}
+
+### Other changes
+${formatMessages(otherCommitMessages)}
+
+### Merges
+${formatMessages(mergeCommitMessages)}
+---
+`;  
+
+    await octokit.pulls.update({
+      owner: github.context.payload.pull_request.base.repo.owner.login,
+      repo: github.context.payload.pull_request.base.repo.name,
+      pull_number: github.context.payload.pull_request.number,
+      body: getUpdatedDescription(oldDescription, newDescription),
+    });
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+})();
